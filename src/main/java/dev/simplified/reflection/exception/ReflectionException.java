c89ac6148683dcc16c4a1a4974e83e9c15c9f2f2
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2021-12-17 17:24:21 -0500

    Fix exception constructor reflection errors

--- src/main/java/dev/simplified/reflection/exception/ReflectionException.java
+++ src/main/java/dev/simplified/reflection/exception/ReflectionException.java
@@ -2,11 +2,16 @@ package dev.sbs.api.reflection.exception;
 
 import dev.sbs.api.SimplifiedException;
 import dev.sbs.api.reflection.Reflection;
+import dev.sbs.api.util.concurrent.ConcurrentMap;
 
 /**
  * {@link ReflectionException ReflectionExceptions} are thrown when the {@link Reflection} class is unable<br>
  * to perform a specific action.
  */
 public final class ReflectionException extends SimplifiedException {
+
+    private ReflectionException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, ConcurrentMap<String, Object> fields) {
+        super(message, cause, enableSuppression, writableStackTrace, fields);
+    }
 
 }
