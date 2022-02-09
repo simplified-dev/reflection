1e5c7622ca5629b8fb3f4aefbb71f23c7c8e4cd0
Nahydrin <6407029+Nahydrin@users.noreply.github.com>
2022-02-08 20:58:00 -0500

    Implement individual command help response

--- src/main/java/dev/simplified/reflection/exception/ReflectionException.java
+++ src/main/java/dev/simplified/reflection/exception/ReflectionException.java
@@ -3,6 +3,7 @@ package dev.sbs.api.reflection.exception;
 import dev.sbs.api.SimplifiedException;
 import dev.sbs.api.reflection.Reflection;
 import dev.sbs.api.util.concurrent.ConcurrentList;
+import dev.sbs.api.util.concurrent.ConcurrentMap;
 import dev.sbs.api.util.tuple.Triple;
 
 /**
@@ -11,8 +12,8 @@ import dev.sbs.api.util.tuple.Triple;
  */
 public final class ReflectionException extends SimplifiedException {
 
-    private ReflectionException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, ConcurrentList<Triple<String, String, Boolean>> fields) {
-        super(message, cause, enableSuppression, writableStackTrace, fields);
+    private ReflectionException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, ConcurrentList<Triple<String, String, Boolean>> fields, ConcurrentMap<String, Object> data) {
+        super(message, cause, enableSuppression, writableStackTrace, fields, data);
     }
 
 }
